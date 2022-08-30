e24109c89a2db65f827e0dc9f0159ed0dadda2b0
Henri Aïdasso <ahenrij@gmail.com>
2022-08-30 13:49:52 +0200

    phone model updated

--- src/models/phone.py
+++ src/models/phone.py
@@ -2,33 +2,28 @@
 
 from datetime import datetime
 from typing import Optional
-from bson import ObjectId
+from beanie import Document
 from pydantic import Field
 
-from src.db import Base
 
-
-class Phone(Base):
+class Phone(Document):
     """Phone model class."""
 
-    phone_number: str = Field(..., alias="phoneNumber", allow_mutation=False)
-    confirmation_token: Optional[str] = Field(..., alias="confirmationToken")
+    phone_number: str = Field(alias="phoneNumber", allow_mutation=False)
+    confirmation_token: Optional[str] = Field(alias="confirmationToken")
     verified: bool = Field(...)
-    created_at: datetime = Field(..., default=datetime.now(), alias="createdAt")
-    updated_at: datetime = Field(..., default=datetime.now(), alias="updatedAt")
+    created_at: datetime = Field(default=datetime.now(), alias="createdAt")
+    updated_at: datetime = Field(default=datetime.now(), alias="updatedAt")
 
     class Config:
         """Phone model config."""
 
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
                 "phone_number": "33 6 00 00 00 00",
                 "verified": False,
                 "confirmation_token": None,
-                "created_at": "",
-                "updated_at": "",
+                "created_at": datetime.now(),
+                "updated_at": datetime.now(),
             }
         }
